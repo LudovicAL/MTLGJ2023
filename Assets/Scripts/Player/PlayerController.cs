f1fda96132d0b3ca9ea8f23065c07f8dafed1dbc
ThunderCork <chrisc@thunderlotusgames.com>
2023-02-10 21:33:20 -0500

    AWESOME CARS

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -12,8 +12,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private List<AxleInfo> axleInfos; // the information about each individual axle
 
-    [SerializeField]
-    private float maxMotorTorque; // maximum torque the motor can apply to wheel
+    //[SerializeField]
+    //private float maxMotorTorque; // maximum torque the motor can apply to wheel
 
     [SerializeField]
     [Range(1.0f, 90.0f)]
@@ -31,8 +31,24 @@ public class PlayerController : MonoBehaviour
     private float brakeToReverseRPMThreshold = 100.0f;
     private float brakeStiffnessOverride = 5.0f;
     private float maxBrakeTorque = 3000;
+    private float firstGearRPMThreshold = 500;
+    private float firstGearStiffness = 5.0f;
+    private float firstGearMotorTorque = 3500.0f;
+    private float secondGearRPMThreshold = 600;
+    private float secondGearMotorTorque = 2500.0f;
+    private float thirdGearMotorTorque = 1500.0f;
+
+    private float currentMotorTorque = 0.0f;
+    private int currentGear = 0;
+    private int gearState = 0; //too lazy for an enum, 1 is accelerate, 0 neutral, -1 is brake, -2 is reverse.
+    private bool isHandbrake = false;
+    private float handbrakeWheelStiffnessOverride = .75f;
+
+    private float secretRocketBoostForce = 50.0f;
 
     public bool showDebugDisplay = true;
+
+    private bool isSecretBoostOn = false;
 
     private void Start()
     {
@@ -78,6 +94,101 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    public void ActivateHandbrake(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.started)
+            isHandbrake = true;
+
+        if (callbackContext.canceled)
+            isHandbrake = false;
+
+        //Debug.Log(callbackContext);
+    }
+
+    private void ResetWheelStiffness(WheelCollider wheelCollider, bool isFrontWheel)
+    {
+        WheelFrictionCurve forwardFrictionCurve = wheelCollider.forwardFriction;
+        if (isFrontWheel)
+        {
+            forwardFrictionCurve.stiffness = frontWheelForwardFrictionOverride;
+        }
+        else
+        {
+            forwardFrictionCurve.stiffness = wheelStiffnessDefault;
+        }
+        wheelCollider.forwardFriction = forwardFrictionCurve;
+
+        WheelFrictionCurve sideFrictionCurve = wheelCollider.sidewaysFriction;
+        if (isFrontWheel)
+        {
+            sideFrictionCurve.stiffness = 1.0f;
+        }
+        else
+        {
+            sideFrictionCurve.stiffness = 1.5f;
+        }
+        wheelCollider.sidewaysFriction = sideFrictionCurve;
+    }
+
+    private void UpdateCurrentGear(float averageRPM)
+    {
+        switch (gearState)
+        {
+            case 1:
+                currentGear = 1;
+                currentMotorTorque = firstGearMotorTorque;
+
+                if (averageRPM > firstGearRPMThreshold)
+                {
+                    currentGear = 2;
+                    currentMotorTorque = secondGearMotorTorque;
+                }
+                if (averageRPM > secondGearRPMThreshold)
+                {
+                    currentGear = 3;
+                    currentMotorTorque = thirdGearMotorTorque;
+                }
+
+                break;
+            case 0:
+                currentGear = 0;
+                currentMotorTorque = 0.0f;
+                break;
+            case -1:
+                currentGear = 0;
+                currentMotorTorque = 0.0f;
+                break;
+            case -2:
+                currentGear = -1;
+                currentMotorTorque = firstGearMotorTorque;
+                break;
+        }
+    }
+
+    private void UpdateWheelStiffnessForAcceleration(WheelCollider wheelCollider, bool isFrontWheel)
+    {
+        WheelFrictionCurve forwardFrictionCurve = wheelCollider.forwardFriction;
+        if (wheelCollider.motorTorque != 0.0f)
+        {
+            /*
+            switch(currentGear)
+            {
+                case 1:
+                    forwardFrictionCurve.stiffness = firstGearStiffness;
+                    break;
+                case 2:
+                    forwardFrictionCurve.stiffness = secondGearStiffness;
+                    break;
+                case 3:
+                    forwardFrictionCurve.stiffness = thirdGearStiffness;
+                    break;
+            }
+            */
+            forwardFrictionCurve.stiffness = firstGearStiffness;
+        }
+        wheelCollider.forwardFriction = forwardFrictionCurve;
+    }
+
     private void UpdateWheelStiffnessForBraking(WheelCollider wheelCollider, bool isFrontWheel)
     {
         WheelFrictionCurve forwardFrictionCurve = wheelCollider.forwardFriction;
@@ -85,18 +196,16 @@ public class PlayerController : MonoBehaviour
         {
             forwardFrictionCurve.stiffness = brakeStiffnessOverride;
         }
-        else
-        {
-            if (isFrontWheel)
-            {
-                forwardFrictionCurve.stiffness = frontWheelForwardFrictionOverride;
-            }
-            else
-            {
-                forwardFrictionCurve.stiffness = wheelStiffnessDefault;
-            }
-        }
         wheelCollider.forwardFriction = forwardFrictionCurve;
+    }
+    private void UpdateWheelStiffnessForHandbrake(WheelCollider wheelCollider)
+    {
+        WheelFrictionCurve sideFrictionCurve = wheelCollider.sidewaysFriction;
+        if (wheelCollider.brakeTorque != 0.0f)
+        {
+            sideFrictionCurve.stiffness = handbrakeWheelStiffnessOverride;
+        }
+        wheelCollider.sidewaysFriction = sideFrictionCurve;
     }
 
     private void Awake()
@@ -107,14 +216,22 @@ public class PlayerController : MonoBehaviour
     private float currentMotorAmount = 0.0f;
     public void FixedUpdate()
     {
+        gearState = 0;
+
         Vector2 inputVector = playerInput.actions["Move"].ReadValue<Vector2>();
 
         currentMotorAmount = inputVector.y;
 
         float steering = maxSteeringAngle * inputVector.x;
-            
-        foreach (AxleInfo axleInfo in axleInfos) {
-            if (axleInfo.steering) {
+        
+        foreach (AxleInfo axleInfo in axleInfos) 
+        {
+            ResetWheelStiffness(axleInfo.leftWheel, axleInfo.wheelType == WheelType.FrontWheel);
+            ResetWheelStiffness(axleInfo.rightWheel, axleInfo.wheelType == WheelType.FrontWheel);
+
+
+            if (axleInfo.steering) 
+            {
                 axleInfo.leftWheel.steerAngle = steering;
                 axleInfo.rightWheel.steerAngle = steering;
             }
@@ -128,22 +245,26 @@ public class PlayerController : MonoBehaviour
                 {
                     if (axleInfo.leftWheel.rpm > -brakeToReverseRPMThreshold)
                     {
-                        axleInfo.leftWheel.motorTorque = currentMotorAmount * maxMotorTorque;
+                        axleInfo.leftWheel.motorTorque = currentMotorAmount * currentMotorTorque;
+                        gearState = 1;
                     }
                     if (axleInfo.rightWheel.rpm > -brakeToReverseRPMThreshold)
                     {
-                        axleInfo.rightWheel.motorTorque = currentMotorAmount * maxMotorTorque;
+                        axleInfo.rightWheel.motorTorque = currentMotorAmount * currentMotorTorque;
+                        gearState = 1;
                     }
                 }
                 else if (currentMotorAmount < 0.0)
                 {
                     if (axleInfo.leftWheel.rpm < brakeToReverseRPMThreshold)
                     {
-                        axleInfo.leftWheel.motorTorque = currentMotorAmount * maxMotorTorque;
+                        axleInfo.leftWheel.motorTorque = currentMotorAmount * currentMotorTorque;
+                        gearState = -2;
                     }
                     if (axleInfo.rightWheel.rpm < brakeToReverseRPMThreshold)
                     {
-                        axleInfo.rightWheel.motorTorque = currentMotorAmount * maxMotorTorque;
+                        axleInfo.rightWheel.motorTorque = currentMotorAmount * currentMotorTorque;
+                        gearState = -2;
                     }
                 }
             }
@@ -158,10 +279,12 @@ public class PlayerController : MonoBehaviour
                     if (axleInfo.leftWheel.rpm < -brakeToReverseRPMThreshold)
                     {
                         axleInfo.leftWheel.brakeTorque = currentMotorAmount * maxBrakeTorque;
+                        gearState = -1;
                     }
                     if (axleInfo.rightWheel.rpm < -brakeToReverseRPMThreshold)
                     {
                         axleInfo.rightWheel.brakeTorque = currentMotorAmount * maxBrakeTorque;
+                        gearState = -1;
                     }
                 }
                 else if (currentMotorAmount < 0.0)
@@ -169,20 +292,64 @@ public class PlayerController : MonoBehaviour
                     if (axleInfo.leftWheel.rpm > brakeToReverseRPMThreshold)
                     {
                         axleInfo.leftWheel.brakeTorque = currentMotorAmount * maxBrakeTorque * -1.0f;
+                        gearState = -1;
                     }
                     if (axleInfo.rightWheel.rpm > brakeToReverseRPMThreshold)
                     {
                         axleInfo.rightWheel.brakeTorque = currentMotorAmount * maxBrakeTorque * -1.0f;
+                        gearState = -1;
                     }
                 }
-
-                UpdateWheelStiffnessForBraking(axleInfo.leftWheel, axleInfo.wheelType == WheelType.FrontWheel);
-                UpdateWheelStiffnessForBraking(axleInfo.rightWheel, axleInfo.wheelType == WheelType.FrontWheel);
             }
-
 
             ApplyLocalPositionToVisuals(axleInfo.leftWheel);
             ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+        }
+
+        float totalRPM = 0.0f;
+        float wheelCount = 0;
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            totalRPM += axleInfo.leftWheel.rpm;
+            totalRPM += axleInfo.rightWheel.rpm;
+            wheelCount += 2;
+        }
+        UpdateCurrentGear(totalRPM / wheelCount);
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (gearState == -1)
+            {
+                UpdateWheelStiffnessForBraking(axleInfo.leftWheel, axleInfo.wheelType == WheelType.FrontWheel);
+                UpdateWheelStiffnessForBraking(axleInfo.rightWheel, axleInfo.wheelType == WheelType.FrontWheel);
+            }
+            else if (gearState == 1)
+            {
+                UpdateWheelStiffnessForAcceleration(axleInfo.leftWheel, axleInfo.wheelType == WheelType.FrontWheel);
+                UpdateWheelStiffnessForAcceleration(axleInfo.rightWheel, axleInfo.wheelType == WheelType.FrontWheel);
+            }
+
+            if (isHandbrake)
+            {
+                if (axleInfo.handbrake)
+                {
+                    axleInfo.leftWheel.brakeTorque = maxBrakeTorque;
+                    axleInfo.rightWheel.brakeTorque = maxBrakeTorque;
+                    UpdateWheelStiffnessForHandbrake(axleInfo.leftWheel);
+                    UpdateWheelStiffnessForHandbrake(axleInfo.rightWheel);
+                }
+            }
+        }
+
+        isSecretBoostOn = false;
+        if (gearState == 1)
+        {
+            Rigidbody rb = transform.GetComponent<Rigidbody>();
+            if (rb.velocity.magnitude < 10.0f)
+            {
+                isSecretBoostOn = true;
+                rb.AddForce(transform.forward * secretRocketBoostForce, ForceMode.Acceleration);
+            }
         }
     }
     
@@ -223,7 +390,22 @@ public class PlayerController : MonoBehaviour
         style.fontSize = 25;
         style.normal.textColor = Color.red;
 
-        string wheelDebugInfo = "MOTOR:  " + currentMotorAmount + "\n";
+        string wheelDebugInfo = "GEARSTATE: " + gearState + "\n" + "GEAR: " + currentGear + "\n" + "MOTOR:  " + currentMotorAmount * currentMotorTorque + "\n";
+
+        Rigidbody rb = transform.GetComponent<Rigidbody>();
+        wheelDebugInfo += "Velocity:  " + rb.velocity.magnitude + "\n";
+
+        wheelDebugInfo += "SECRETBOOST:  ";
+        if (isSecretBoostOn)
+        {
+            wheelDebugInfo += "ON";
+        }
+        else
+        {
+            wheelDebugInfo += "OFF";
+        }
+        wheelDebugInfo += "\n";
+
         foreach (AxleInfo axleInfo in axleInfos)
         {
             wheelDebugInfo += axleInfo.leftWheel.name + "\n";
