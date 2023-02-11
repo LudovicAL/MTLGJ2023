5c72c3276952d69a2ff46eabc65ca88fa4cac2b4
Ludovic <ludovicaubut@hotmail.com>
2023-02-10 21:27:54 -0500

    Car sounds

--- Assets/Scripts/CarSounds.cs
+++ Assets/Scripts/CarSounds.cs
@@ -3,18 +3,12 @@ using UnityEngine;
 public class CarSounds : MonoBehaviour {
 
     [SerializeField]
-    private bool useSounds = false;
-    [SerializeField]
-    private AudioSource carEngineSound;
-    [SerializeField]
-    private AudioSource tireScreechSound;
-    [SerializeField]
     private float initialCarEnginePitch;
     [SerializeField]
+    private float minimumSpeedForDriftingSounds;
+
     private AudioSource engineAudioSource;
-    [SerializeField]
     private AudioSource tireScreechAudioSource;
-    [SerializeField]
     private Rigidbody carRigidbody;
 
     private bool isDrifting = false;
@@ -22,6 +16,15 @@ public class CarSounds : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
+        carRigidbody = GetComponent<Rigidbody>();
+        engineAudioSource = transform.Find("AudioSource Engine").GetComponent<AudioSource>();
+        tireScreechAudioSource = transform.Find("AudioSource TireScreeching").GetComponent<AudioSource>();
+        if (engineAudioSource) {
+            engineAudioSource.gameObject.SetActive(true);
+        }
+        if (tireScreechAudioSource) {
+            tireScreechAudioSource.gameObject.SetActive(true);
+        }
         engineAudioSource.pitch = initialCarEnginePitch;
         InvokeRepeating("PlayCarSounds", 0f, 0.1f);
     }
@@ -32,25 +35,18 @@ public class CarSounds : MonoBehaviour {
     }
 
     private void PlayCarSounds() {
-        if (useSounds) {
-            float carSpeed = Mathf.Abs(carRigidbody.velocity.magnitude);
-            if (carEngineSound != null) {
-                carEngineSound.pitch = Mathf.Clamp(initialCarEnginePitch + (carSpeed / 25f), 0.0f, 3.0f);
-                carEngineSound.volume = Mathf.Clamp01(carSpeed / (carMaxSpeed / 2.0f));
+        float carSpeed = Mathf.Abs(carRigidbody.velocity.magnitude);
+        if (engineAudioSource != null) {
+            engineAudioSource.pitch = Mathf.Clamp(initialCarEnginePitch + (carSpeed / 25f), 0.0f, 3.0f);
+            engineAudioSource.volume = Mathf.Clamp01(carSpeed / (carMaxSpeed / 2.0f));
+        }
+        if (isDrifting && carSpeed > minimumSpeedForDriftingSounds) {
+            if (!tireScreechAudioSource.isPlaying) {
+                tireScreechAudioSource.Play();
             }
-            if (isDrifting || (carSpeed > 12f)) {
-                if (!tireScreechSound.isPlaying) {
-                    tireScreechSound.Play();
-                }
-            } else if ((!isDrifting) && (carSpeed < 12f)) {
-                tireScreechSound.Stop();
-            }
-        } else if (!useSounds) {
-            if (carEngineSound != null && carEngineSound.isPlaying) {
-                carEngineSound.Stop();
-            }
-            if (tireScreechSound != null && tireScreechSound.isPlaying) {
-                tireScreechSound.Stop();
+        } else if (!isDrifting || carSpeed < minimumSpeedForDriftingSounds) {
+            if (tireScreechAudioSource.isPlaying) {
+                tireScreechAudioSource.Stop();
             }
         }
     }
