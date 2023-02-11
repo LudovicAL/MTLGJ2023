d7bf59d7b4d0c26ce7fe87d0271afde73aa39efb
Sébastien Ducharme <sebastiend@thunderlotusgames.com>
2023-02-11 16:57:57 -0500

    Bench destructibles, mesh colliders on trottoires

---
+++ Assets/Scripts/Destructible.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Destructible : MonoBehaviour
+{
+    [SerializeField] private float impactMultiplier = 100;
+    
+    private GameObject playerObject;
+    private Rigidbody carRigidbody;
+    private Rigidbody destructibleRigidBody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerObject = FindObjectOfType<PlayerController>().gameObject;
+        carRigidbody = playerObject.GetComponent<Rigidbody>();
+        destructibleRigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject != playerObject)
+            return;
+
+        Debug.Log("hit");
+
+        Vector3 carVelocity = carRigidbody.velocity;
+        Vector3 force = new Vector3(carVelocity.x, Random.Range(5, 10), carVelocity.z);
+        destructibleRigidBody.AddForceAtPosition(force, other.GetContact(0).point, ForceMode.Impulse);
+    }
+}
