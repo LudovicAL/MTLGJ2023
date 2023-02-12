9d9089ed489949214ee95636281990d4421d6e8a
Ludovic <ludovicaubut@hotmail.com>
2023-02-12 11:01:23 -0500

    Fine tuning xp requirements

--- Assets/Scripts/PlayerLevelManager.cs
+++ Assets/Scripts/PlayerLevelManager.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.InputSystem.Controls;
+using System.Text;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class PlayerLevelManager : Singleton<PlayerLevelManager> {
 
@@ -14,14 +17,14 @@ public class PlayerLevelManager : Singleton<PlayerLevelManager> {
     public int xpRequiredForNextLevel { get; private set; } = int.MaxValue;
 
     private void Start() {
-        xpRequiredForNextLevel = GetXpRequiredForNextLevel();
+        xpRequiredForNextLevel = GetXpRequiredForLevel(currentLevel + 1);
         PlayerData.Instance.Xp.OnCurrentChanged.AddListener(XpChanged);
     }
 
     private void LevelUp() {
         currentLevel++;
         xpAtStartOfLevel = xpRequiredForNextLevel;
-        xpRequiredForNextLevel = GetXpRequiredForNextLevel();
+        xpRequiredForNextLevel = GetXpRequiredForLevel(currentLevel + 1);
         playerLeveledUpEvent.Invoke();
     }
 
@@ -35,7 +38,32 @@ public class PlayerLevelManager : Singleton<PlayerLevelManager> {
         return (int)(Mathf.Sqrt(PlayerData.Instance.Xp.Current) * constant);
     }
 
-    public int GetXpRequiredForNextLevel() {
-        return Mathf.RoundToInt(Mathf.Pow(((currentLevel + 1) / constant), 2));
+    public int GetXpRequiredForLevel(int level) {
+        return Mathf.RoundToInt(Mathf.Pow((level / constant), 2));
     }
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(PlayerLevelManager))]
+public class PlayerLevelManagerEditor : Editor {
+    public override void OnInspectorGUI() {
+        var playerLevelManager = (PlayerLevelManager)target;
+        if (playerLevelManager == null) {
+            return;
+        }
+
+        if (GUILayout.Button("Log xp required for each levels")) {
+            int XpRequiredForPreviousLevel = 0;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0, max = 30; i < max; i++) {
+                int XpRequiredForCurrentLevel = playerLevelManager.GetXpRequiredForLevel(i);
+                sb.Append("Level " + i + ": " + XpRequiredForCurrentLevel
+                    + "xp (Diff: " + (XpRequiredForCurrentLevel - XpRequiredForPreviousLevel) + ")\n");
+                XpRequiredForPreviousLevel = XpRequiredForCurrentLevel;
+            }
+            Debug.Log(sb.ToString());
+        }
+        DrawDefaultInspector();
+    }
+}
+#endif
