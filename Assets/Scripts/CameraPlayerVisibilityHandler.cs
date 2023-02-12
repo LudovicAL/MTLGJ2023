b55b454645760ab3a3d9ebbe7d87df18bd1a0d66
Sébastien Ducharme <sebastiend@thunderlotusgames.com>
2023-02-11 20:23:44 -0500

    Big camera update! Camera doesn't match player orientation, building occlusion (WIP, part 2 coming soon)

---
+++ Assets/Scripts/CameraPlayerVisibilityHandler.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayerVisibilityHandler : MonoBehaviour
+{
+    class BuildingInfo
+    {
+        public GameObject building;
+        public Material previousMaterial;
+        public bool hitThisFrame;
+    }
+
+    [SerializeField] Material hiddenMaterial;
+    
+    private GameObject player;
+    private Camera camera;
+
+    private int layerMask;
+    private RaycastHit[] hitResults = new RaycastHit[5];
+
+    private Dictionary<int, BuildingInfo> hiddenBuildings = new Dictionary<int, BuildingInfo>();
+    private List<int> toRemoveFromHidden = new List<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>().gameObject;
+        camera = Camera.main;
+
+        layerMask = 1 << 7;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 cameraPos = camera.transform.position;
+        Vector3 playerPos = player.transform.position;
+        Vector3 playerToCamera = cameraPos - playerPos;
+        Vector3 playerToCameraNormalized = playerToCamera.normalized;
+
+        foreach (BuildingInfo info in hiddenBuildings.Values)
+            info.hitThisFrame = false;
+        
+        int size = Physics.RaycastNonAlloc(playerPos, playerToCameraNormalized, hitResults, playerToCamera.magnitude, layerMask);
+        for (int i = 0; i < size; i++)
+        {
+            RaycastHit hit = hitResults[i];
+            GameObject hitObject = hit.transform.gameObject;
+            int objectInstanceID = hitObject.GetInstanceID();
+            
+            if (!hiddenBuildings.ContainsKey(objectInstanceID))
+            {
+                MeshRenderer buildingRenderer = hitObject.GetComponent<MeshRenderer>();
+
+                hiddenBuildings.Add(objectInstanceID, new BuildingInfo
+                {
+                    building = hitObject,
+                    previousMaterial = buildingRenderer.material,
+                    hitThisFrame = true
+                });
+
+                // change material to hidden
+                buildingRenderer.material = hiddenMaterial;
+            }
+            else
+            {
+                hiddenBuildings[objectInstanceID].hitThisFrame = true;
+            }
+        }
+
+        toRemoveFromHidden.Clear();
+        foreach (int key in hiddenBuildings.Keys)
+        {
+            BuildingInfo info = hiddenBuildings[key];
+            if (info.hitThisFrame)
+                continue;
+
+            info.building.GetComponent<MeshRenderer>().material = info.previousMaterial;
+            
+            toRemoveFromHidden.Add(key);
+        }
+
+        foreach (int key in toRemoveFromHidden)
+            hiddenBuildings.Remove(key);
+    }
+}
